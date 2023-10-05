f1db7b2573fbc79a0343276ae69185bae33377a4
Vishnu Vardhan <vishnu@MacBook-Pro.local>
2023-10-05 15:28:50 +0530

    basic editor

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <filename>", args[0]);
+        return Ok(());
+    }
+
+    let filename = &args[1];
+
+    // Check if the file exists, and if not, create it.
+    if !fs::metadata(filename).is_ok() {
+        println!("Creating a new file: {}", filename);
+        fs::write(filename, "")?;
+    }
+
+    loop {
+        println!("Enter your text (Ctrl+C to exit):");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input)?;
+
+        let mut file = fs::OpenOptions::new()
+            .write(true)
+            .append(true)
+            .open(filename)?;
+
+        file.write_all(input.as_bytes())?;
+    }
 }
